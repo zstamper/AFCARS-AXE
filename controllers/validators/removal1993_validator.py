2dfaff1d98afbd194c3b10b1d2cffada6f5dc2d9
John Burkhard <john.burkhard@icf.com>
2024-02-14 18:31:41 -0500

    Issue 62 1993 removals are all required

--- controllers/validators/removal1993_validator.py
+++ controllers/validators/removal1993_validator.py
@@ -8,6 +8,7 @@ class Removal1993BaseValidator:
 
     def __init__(self, dialog: Removal1993Dialog):
         self.dialog = dialog
+        self.parent_data = None
 
     @staticmethod
     def is_valid_date(d: int) -> bool:
@@ -23,13 +24,13 @@ class Removal1993BaseValidator:
 
     @staticmethod
     def is_future_date(d: int) -> bool:
+        if d is None:
+            return False
         year = d // 10000
         month = (d - (d // 10000) * 10000) // 100
-        day = d - (d//100)*100
+        day = d - (d // 100) * 100
         d = date(year=year, month=month, day=day)
         return d > date.today()
-
-
 
 
 class Removal1993Validators(Removal1993BaseValidator):
@@ -39,6 +40,8 @@ class Removal1993Validators(Removal1993BaseValidator):
             e69 = self.dialog.e69
         except ValueError:
             raise ValueError("Removal Date (E69) is invalid format.")
+        if self.dialog.e69 is None:
+            raise ValueError("Removal Date (E69) is required.")
         if not self.is_valid_date(self.dialog.e69):
             raise ValueError("Invalid date provided for Removal Date (E69).")
         if self.is_future_date(self.dialog.e69):
@@ -49,6 +52,8 @@ class Removal1993Validators(Removal1993BaseValidator):
             e153 = self.dialog.e153
         except ValueError:
             raise ValueError("Exit Date (E153) is invalid format.")
+        if self.dialog.e153 is None:
+            raise ValueError("Exit Date (E153) is required.")
         if self.dialog.e153 and not self.is_valid_date(self.dialog.e153):
             raise ValueError("Invalid date provided for Exit Date (E153).")
         if self.is_future_date(self.dialog.e153):
@@ -69,6 +74,14 @@ class Removal1993Validator(AbstractValidator):
         self.dialog = dialog
         self.validator = Removal1993Validators(self.dialog)
         self._messages: list[ValueError] = []
+
+    @property
+    def parent_data(self):
+        return self.validator.parent_data
+
+    @parent_data.setter
+    def parent_data(self, v):
+        self.validator.parent_data = v
 
     def validate(self) -> bool:
         results = []
