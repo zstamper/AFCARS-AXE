113a34c4222db31b7e4dd7ae2f24908687ada9a4
John Burkhard <john.burkhard@icf.com>
2024-02-14 14:53:09 -0500

    Issue 50 fix validators

--- controllers/validators/permanency_plan_validators.py
+++ controllers/validators/permanency_plan_validators.py
@@ -15,10 +15,23 @@ class PermanencyPlanBaseValidator:
             year = int(s[0:4])
             month = int(s[4:6])
             day = int(s[6:8])
-            date(year=year, month=month, day=day)
+            d = date(year=year, month=month, day=day)
+            today = date.today()
+            min_d = date(year=today.year - 100, month=today.month, day=today.day)
+            assert d >= min_d
             return True
         except ValueError:
             return False
+        except AssertionError:
+            return False
+
+    @staticmethod
+    def is_future_date(d: int) -> bool:
+        year = d // 10000
+        month = (d - (d // 10000) * 10000) // 100
+        day = d - (d//100)*100
+        d = date(year=year, month=month, day=day)
+        return d > date.today()
 
 
 class PermanencyPlanValidators(PermanencyPlanBaseValidator):
@@ -26,6 +39,8 @@ class PermanencyPlanValidators(PermanencyPlanBaseValidator):
     def validate_e147(self):
         if not self.is_valid_date(self.dialog.e147):
             raise ValueError(f"Permanency Plan date (E147) is missing or invalid.")
+        if self.is_future_date(self.dialog.e147):
+            raise ValueError("Permanency Plan Date (E147) cannot be in the future.")
 
     def validate_e148(self):
         if self.dialog.e148 not in [1, 2, 3, 4, 5]:
